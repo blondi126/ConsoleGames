5180d351f6cec72e0967d91f94ffc54cc3d396d7
Maksim Udincev <maksim.udincev01@mail.ru>
2022-03-17 12:46:08 +0300

    add gallows game

---
+++ Gallows/Gallows.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Gallows
+{
+    public class Gallows
+    {
+        private readonly string[] _dictionary = File.ReadAllLines("D:\\Studies\\Course_Udemy_CS\\Gallows\\WordsStockRus.txt");
+        public string Word;
+        public string GuessedWord { get; set; }
+        public int AttemptsCounter = 6;
+        public int RightAns = 0;
+
+       public Gallows()
+        {
+            var randomInt = new Random();
+            var num = randomInt.Next(0, _dictionary.Length);
+            Word = _dictionary[num];
+            var tmp = _dictionary[num];
+            var tmp0 = tmp.ToCharArray();
+            for (var i = 0; i < Word.Length; i++) tmp0[i] = '-';
+            GuessedWord = new string(tmp0);
+        }
+        public bool FindAndGetLetter(char letter)
+        {
+            var ret = false;
+            for (var i =0; i<Word.Length; i++)
+            {
+                if (Word[i] != letter) continue;
+
+                var tmp0 = GuessedWord.ToCharArray();
+                tmp0[i] = letter;
+                GuessedWord = new string(tmp0);
+                ret = true;
+                RightAns++;
+            }
+            if(!ret)AttemptsCounter--;
+            return ret;
+        }
+    }
+}
---
+++ Gallows/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Gallows
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var game = new Gallows();
+            Console.WriteLine("We asked the word. Try to guess it.");
+            Console.WriteLine(game.GuessedWord);
+            Console.WriteLine("\nPlease, enter a lowercase russian letter.");
+            while (game.AttemptsCounter > 0 && game.RightAns < game.Word.Length)
+            {
+                char letter;
+                while (!char.TryParse(Console.ReadLine(), out letter))
+                    Console.WriteLine("Enter only lowercase russian letter");
+
+                if (game.FindAndGetLetter(letter))
+                {
+                    Console.WriteLine($"\nYou guessed the letter:\n{game.GuessedWord}\n");
+                    if (game.RightAns < game.Word.Length) Console.WriteLine("Enter 1 more letter");
+                }
+                else if (game.AttemptsCounter > 0) Console.WriteLine("\nThere is no such letter in the word. Try again:");
+            }
+
+            Console.WriteLine(game.RightAns == game.Word.Length
+                ? $"You win. Congratulations. You made {6 - game.AttemptsCounter} mistakes."
+                : $"\nYou lost. You made 6 mistakes. It was the word {game.Word}\n");
+        }
+    }
+}
